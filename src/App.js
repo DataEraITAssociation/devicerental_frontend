e8bf991ec0bf7bf41c0af421ed8b90c0fb84d546
zhouuualexander <zzhou3@ualberta.ca>
2021-08-03 10:02:54 -0600

    Developed one of products with product name, image, available quantity, and rate for rent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import ProductCard from '../src/Components/ProductCard/ProductCard';
+import Typography from '@material-ui/core/Typography';
+
+const machineList = {
+  machineOne: {
+    title: 'Machine One',
+    quality: '2',
+    image: 'public/machineOne.jpeg',
+    rate: '$14.99',
+    imageName: 'Machine One',
+    description: 'This machine is great',
+  }
+};
 function App() {
   return (
     <React.Fragment className="App">
       <CssBaseline />
+      <Typography variant="h3" align="center">
+        Product Rental
+      </Typography>
+      <ProductCard data={machineList} />
     </React.Fragment>
   );
 }
