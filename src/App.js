8cf505a7695f0455599b7258df5295952bc19d53
zhouuualexander <zzhou3@ualberta.ca>
2021-08-03 23:51:26 -0600

    finished feature 9, except the table of avaibility

--- src/App.js
+++ src/App.js
@@ -9,33 +9,45 @@ import machineThree from '../src/Assets/Images/machineThree.jpeg';
 import machineFour from '../src/Assets/Images/machineFour.jpeg';
 const machineList = [{
   title: 'Machine One',
-  quality: '2',
+  quantity: '2',
   image: machineOne,
   rate: '$14.99',
   imageName: 'Machine One',
   description: 'This machine is great',
+  detailedDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi quis eleifend quam adipiscing vitae proin sagittis. Vulputate eu scelerisque felis imperdiet proin fermentum.',
+  tutorial: 'https://en.wikipedia.org/wiki/Robot',
+  youTube: 'https://www.youtube.com/embed/Bg_tJvCA8zw'
 },
 {
   title: 'Machine Two',
-  quality: '10',
+  quantity: '10',
   image: machineTwo,
   rate: '$12.99',
   imageName: 'Machine Two',
-  description: 'This machine is best!'
+  description: 'This machine is best!',
+  detailedDescription: 'Elementum curabitur vitae nunc sed velit dignissim sodales. Posuere ac ut consequat semper. Lectus magna fringilla urna porttitor rhoncus dolor purus non enim. Tristique senectus et netus et malesuada fames. Quisque egestas diam in arcu.',
+  tutorial: 'https://www.ft.com/content/0bba6e2b-bce9-4153-8b52-1abfd6e921a5',
+  youTube: 'https://www.youtube.com/embed/m-LP4qpOLl0'
 }, {
   title: 'Machine Three',
-  quality: '100',
+  quantity: '100',
   image: machineThree,
   rate: '$107.99',
   imageName: 'Machine Three',
-  description: 'Please rent this one if you are kids!'
+  description: 'Please rent this one if you are kids!',
+  detailedDescription: 'Iaculis urna id volutpat lacus. Convallis convallis tellus id interdum velit laoreet id donec. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Venenatis cras sed felis eget velit aliquet. Non blandit massa enim nec dui. ',
+  tutorial: 'https://spectrum.ieee.org/video-friday-telexistence-model-t-robot',
+  youTube: 'https://www.youtube.com/embed/RNVh_HMX2IY'
 }, {
   title: 'Machine Four',
-  quality: '76',
+  quantity: '76',
   image: machineFour,
   rate: '$56.99',
   imageName: 'Machine Four',
-  description: 'Please rent this one if you are adults!'
+  description: 'Please rent this one if you are adults!',
+  detailedDescription: 'Tortor condimentum lacinia quis vel eros. Netus et malesuada fames ac. Lacus luctus accumsan tortor posuere ac ut consequat semper. Tempor commodo ullamcorper a lacus.',
+  tutorial: 'https://spectrum.ieee.org/',
+  youTube: 'https://www.youtube.com/embed/a_T4QayqtI4'
 }
 
 ];
